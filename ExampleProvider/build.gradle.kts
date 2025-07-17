ff18bece0f08e3fe19aa520b309bcdebd427fc29
aravinddwara <149893688+aravinddwara@users.noreply.github.com>
2025-07-17 08:42:25 +0530

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -3,28 +3,17 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
 
-// Use an integer for version numbers
-version = 1
+plugins {
+    id("cloudstream.plugin")
+}
 
+version = "1.0.0"
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
+    language = "ta"
+    description = "Watch Tamil serials and shows from Vijay TV, Sun TV, Zee Tamil, and more."
+    authors = listOf("YourName")
+}
 
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
 
     // Random CC logo I found
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
