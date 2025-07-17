7c95abb13a43cab5a5fd3b99ce01ff24fb10a325
aravinddwara <149893688+aravinddwara@users.noreply.github.com>
2025-07-17 08:46:33 +0530

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,22 +1,23 @@
+plugins {
+    id("cloudstream.plugin") version "0.0.1"
+}
+
 dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
 
-plugins {
-    id("cloudstream.plugin")
-}
+version = 1
 
-version = "1.0.0"
 cloudstream {
     language = "ta"
-    description = "Watch Tamil serials and shows from Vijay TV, Sun TV, Zee Tamil, and more."
-    authors = listOf("YourName")
-}
+    description = "Tamil TV serials and shows from TamilDhool with Dailymotion support"
+    authors = listOf("TamilDhool")
 
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+    status = 1
+    tvTypes = listOf("TvSeries", "Movie")
+    iconUrl = "https://www.tamildhool.net/wp-content/uploads/2020/08/cropped-tamildhool-favicon-32x32.png"
+    requiresResources = false // Set to true if your plugin has assets
 }
 
 android {
